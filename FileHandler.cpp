2264a7a01a5a953b390998a28c78d304e81b3b40
Jack Yu <yuwenjin@umich.edu>
2020-10-20 14:06:32 -0400

    create new files for reading in csv files

---
+++ FileHandler.cpp
@@ -0,0 +1,61 @@
+//GlowCurveAnalysis
+//Writen by Jack Yu UROP 2020 Fall
+
+#include "FileHandler.hpp"
+
+using namespace std;
+//this needs C++17 or above
+namespace fs = std::filesystem;
+
+//open up the input dir directory and find all csv files
+//return a vector of string that contains all the paths to csv files
+//update output_dir to be the local directory for output folder
+vector<string> handle_dir(string dir, string& output_dir) {
+
+	int numDir = 0;
+	int numCSV = 0;
+	vector<string> csv;
+
+	//check if the directory actually exist
+	try {
+		if (!fs::exists(dir)) {
+			throw dir + " Invalid directory path!";
+		}
+	}
+	catch (string str) {
+		cerr << str << endl;
+	}
+
+	//assigned the output folder path to output_dir
+    output_dir = dir + "_output";
+    fs::create_directories(output_dir);
+
+	//iteratively read in all the files/sub-directories in the "dir" directory
+	for (const auto& entry : fs::recursive_directory_iterator(dir)) {
+		//if find sub-directory then record the number of sub-directories found
+		if (fs::is_directory(entry)) {
+			numDir++;
+		}
+		//if it's csv/xlsx file then add to the output vector and increment number of csv found
+		else {
+            //cast the path and check the format
+			string path = entry.path().string();
+			unsigned long dot = path.find_last_of('.');
+			string type = path.substr(dot + 1);
+			if (type == "xlsx" || type == "csv") {
+				csv.push_back(path);
+				numCSV++;
+			}
+		}
+	}
+    //output the details about the directory readed in
+	cout << "Directories found: " << numDir << endl;
+	cout << "CSV files found: " << numCSV << endl;
+	cout << "Output Directory created: " << output_dir << endl;
+	cout << "CSV details: " << endl;
+    //for debugging purpose, shows all the files read
+	for (string s : csv) {
+		cout << s << endl;
+	}
+	return csv;
+}
---
+++ FileHandler.hpp
@@ -0,0 +1,20 @@
+//GlowCurveAnalysis
+//Writen by Jack Yu UROP 2020 Fall
+
+#ifndef newHandler_hpp
+#define newHandler_hpp
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+//filesystem is available in C++17, please specify this c++ version
+//if a compiler like visual studio is used. If using makefile then no changes needed
+#include <filesystem>
+using namespace std;
+
+//open up the dir directory and store all the csv file names
+//update output_dir to be the local directory for output folder
+std::vector<std::string> handle_dir(std::string dir, std::string& output_dir);
+#endif /* newHandler_hpp */
+
+
