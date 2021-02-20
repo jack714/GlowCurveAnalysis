b33607b2c7b42c0d640ef95c9dcda3ac201c7092
jack714 <55667330+jack714@users.noreply.github.com>
2021-02-20 17:42:50 -0500

    gd types test

--- main1.cpp
+++ main1.cpp
@@ -170,6 +170,163 @@ void gd(const vector<double>& temp, const vector<double>& curve, vector<vector<d
     f1 << " new fom: " << FOM << " " << iteration << " times" << endl;
 }
 
+int gd_types(const vector<double>& temp, const vector<double>& curve, vector<vector<double>>& peakParams, double& FOM, int type) {
+    vector<vector<double>> change_range;
+    if (type == 100)
+        change_range = { {0.1, 0.05, 0.05}, {0.15, 0.04, 0.05}, {0.16, 0.03, 0.05} };
+    else if (type == 200)
+        change_range = { {0.16, 0.03, 0.05}, {0.18, 0.02, 0.05}, {0.4, 0.02, 0.05}, {0.3, 0.02, 0.05}, {0.15, 0.03, 0.05}, {0.2, 0.013, 0.05}, {0.18, 0.02, 0.05}, {0.2, 0.02, 0.05}, {0.18, 0.02, 0.05} };
+    else if (type == 300)
+        change_range = { {0.3, 0.03, 0.05}, {0.23, 0.02, 0.05}, {0.4, 0.03, 0.05}, {0.37, 0.03, 0.05}, {0.24, 0.023, 0.05}, {0.2, 0.012, 0.05}, {0.3, 0.024, 0.05}, {0.24, 0.023, 0.05} };
+    else if (type == 400)
+        change_range = { {0.15, 0.014, 0.05}, {0.21, 0.015, 0.05}, {0.18, 0.013, 0.05} };
+    else if (type == 700)
+        change_range = { {0.15, 0.03, 0.05}, {0.15, 0.017, 0.05}, {0.14, 0.02, 0.05}, {0.06, 0.013, 0.05} };
+    else
+        change_range = { {0.12, 0.035, 0.05}, {0.17, 0.016, 0.05}, {0.24, 0.08, 0.05}, {0.14, 0.026, 0.05}, {0.155, 0.012, 0.05}, {0.15, 0.02, 0.05}, {0.25, 0.053, 0.05} };
+    int curveSize = int(curve.size());
+    int peakNum = int(peakParams.size());
+    //find the half max index
+    vector<double> temperature = temp;
+    find_index(temperature, peakParams);
+    //calculate original integral
+    double orig_integral = 0.0;
+    vector<double> orig_fit(curve.size());
+    for (int d = 0; d < int(curve.size()); d++) {
+        double fit = 0.0;
+        for (int e = 0; e < curveSize; e++) {
+            fit += func(temp[d], peakParams[e]);
+        }
+        orig_integral += fit;
+        orig_fit[d] = fit;
+    }
+    double orig_fom = 0.0;
+    for (int f = 0; f < int(curve.size()); ++f) {
+        orig_fom += abs(curve[f] - orig_fit[f]) / orig_integral;
+    }
+    vector<vector<double>> temp_params = peakParams;
+    double k = .000086173303;
+    double rate1 = 0.0000001;
+    double rate2 = 0.00002;
+    double rate3 = 0.00001;
+    double current_FOM = orig_fom;
+    int iteration = 0;
+    //bool matrix to check if should apply change
+    vector<vector<bool>> process(curveSize, vector<bool>(3, true));
+    //check fom
+    bool check = true;
+    //check if any still inside allowable range
+    bool continue_process = true;
+    while (iteration < 500 && check && continue_process) {
+        vector<vector<double>> update(peakNum, vector<double>(3, 0.0));
+        vector<vector<double>> curves(temp_params.size(), vector<double>(curve.size()));
+        vector<double> totalCurve(curve.size());
+        for (int i = 0; i < int(curve.size()); ++i) {
+            double out;
+            double accum = 0.0;
+            for (int x = 0; x < int(temp_params.size()); ++x) {
+                out = quickFok(temp[i], temp_params[x]);
+                curves[x][i] = out;
+                accum += out;
+            }
+            totalCurve[i] = accum;
+        }
+        //use FWHM to find the left and right half max points
+        for (int b = 0; b < peakNum; b++) {
+            int TL = temp_params[b][3];
+            int TR = temp_params[b][5];
+            double energy = temp_params[b][0];
+            double Tm = temp_params[b][1];
+            double Im = temp_params[b][2];
+            for (int index = TL; index < TR + 1; index++) {
+                double y = curve[index] - totalCurve[index] + curves[b][index];
+                if (y < 0)
+                    y = 0;
+                double T = temp[index];
+                double deriv_E = -2.0 * Im * (-(2.0 * k * T * T * T * exp((energy * (T - Tm)) / (Tm * k * T))) / (energy * energy * Tm * Tm) + (2.0 * Tm * k) /
+                    (energy * energy) - (T * (T - Tm) * (1.0 - (2.0 * k * T) / energy) * exp((energy * (T - Tm)) / (Tm * k * T))) / (Tm * Tm * Tm * k) + (T - Tm) /
+                    (Tm * k * T)) * exp(-(T * T * (1.0 - (2.0 * k * T) / energy) * exp((energy * (T - Tm)) / (Tm * k * T))) / (Tm * Tm) + (energy * (T - Tm)) /
+                    (Tm * k * T) - (2.0 * Tm * k) / energy + 1.0) * (y - Im * exp(-(T * T * (1.0 - (2.0 * k * T) / energy) * exp((energy * (T - Tm)) / (Tm * k * T))) /
+                        (Tm * Tm) + (energy * (T - Tm)) / (Tm * k * T) - (2.0 * Tm * k) / energy + 1.0));
+                double deriv_Tm = -2.0 * Im * ((2.0 * T * T * (1.0 - (2.0 * k * T) / energy) * exp((energy * (T - Tm)) / (Tm * k * T))) / (Tm * Tm * Tm) - (T * T * (1.0 - (2.0 * k * T) / energy)
+                    * exp((energy * (T - Tm)) / (Tm * k * T)) * (-(energy * (T - Tm)) / (Tm * Tm * k * T) - energy / (Tm * k * T))) / (Tm * Tm) - (energy * (T - Tm)) / (Tm * Tm * k * T) -
+                    energy / (Tm * k * T) - (2.0 * k) / energy) * exp(-(T * T * (1.0 - (2.0 * k * T) / energy) * exp((energy * (T - Tm)) / (Tm * k * T))) / (Tm * Tm) + (energy * (T - Tm)) / (Tm * k * T) -
+                    (2.0 * Tm * k) / energy + 1.0) * (y - Im * exp(-(T * T * (1.0 - (2.0 * k * T) / energy) * exp((energy * (T - Tm)) / (Tm * k * T))) / (Tm * Tm) + (energy * (T - Tm)) / (Tm * k * T) -
+                        (2.0 * Tm * k) / energy + 1.0));
+                double deriv_Im = -2.0 * exp(-(T * T * (1.0 - (2.0 * k * T) / energy) * exp((energy * (T - Tm)) / (Tm * k * T))) / (Tm * Tm) + (energy * (T - Tm)) /
+                    (Tm * k * T) - (2.0 * Tm * k) / energy + 1.0) * (y - Im * exp(-(T * T * (1.0 - (2.0 * k * T) / energy) * exp((energy * (T - Tm)) / (Tm * k * T))) /
+                    (Tm * Tm) + (energy * (T - Tm)) / (Tm * k * T) - (2.0 * Tm * k) / energy + 1.0));
+                update[b][0] += rate1 * deriv_E;
+                update[b][1] += rate2 * deriv_Tm;
+                update[b][2] += rate3 * deriv_Im;
+            }
+        }
+        //apply changes and check if out range
+        for (int c = 0; c < peakNum; c++) {
+            if (process[c][0]) {
+                temp_params[c][0] -= update[c][0];
+                if ((abs(temp_params[c][0] - peakParams[c][0]) / peakParams[c][0]) > change_range[c][0]) {
+                    //revert
+                    temp_params[c][0] += update[c][0];
+                    //stop process it
+                    process[c][0] = false;
+                }
+            }
+            if (process[c][1]) {
+                temp_params[c][1] -= update[c][1];
+                if ((abs(temp_params[c][1] - peakParams[c][1]) / peakParams[c][1]) > change_range[c][1]) {
+                    //revert
+                    temp_params[c][1] += update[c][1];
+                    //stop process it
+                    process[c][1] = false;
+                }
+            }
+            if (process[c][2]) {
+                temp_params[c][2] -= update[c][2];
+                if ((abs(temp_params[c][2] - peakParams[c][2]) / peakParams[c][2]) > change_range[c][2]) {
+                    //revert
+                    temp_params[c][2] += update[c][2];
+                    //stop process it
+                    process[c][2] = false;
+                }
+            }
+        }
+        //check if all process is false
+        continue_process = false;
+        for (vector<bool>& b : process) {
+            for (int i = 0; i < 3; i++) {
+                if (b[i])
+                    continue_process = true;
+            }
+        }
+        //calculate new fom
+        double new_integral = 0.0;
+        vector<double> new_output(curve.size(), 0.0);
+        for (int d = 0; d < curveSize; d++) {
+            double new_fit = 0.0;
+            for (int e = 0; e < peakNum; e++) {
+                new_fit += func(temp[d], temp_params[e]);
+            }
+            new_integral += new_fit;
+            new_output[d] = new_fit;
+        }
+        double new_fom = 0.0;
+        for (int f = 0; f < curveSize; ++f) {
+            new_fom += abs(curve[f] - new_output[f]) / new_integral;
+        }
+        if (new_fom > current_FOM)
+            check = false;
+        else
+            current_FOM = new_fom;
+        cout << ".";
+        cout.flush();
+        iteration++;
+    }
+    FOM = current_FOM;
+    peakParams = temp_params;
+    return iteration;
+}
+
 int main(int argc, char* argv[]) {
     //enable quick mode to run output with machine generated peak detections
     bool output_mode = false;
@@ -382,73 +539,109 @@ int main(int argc, char* argv[]) {
         //peak_param.push_back({ 1.32, 323.85, 6.2});
         //peak_param.push_back({ 1.36, 349.85, 4.8});
         //TLD 900
-        peak_param.push_back({ 1.02, 122.85, 0.38});
-        peak_param.push_back({ 0.96, 149.85, 0.40});
-        peak_param.push_back({ 1.05, 161.85, 0.65});
-        peak_param.push_back({ 1.40, 188.85, 0.55});
-        peak_param.push_back({ 1.42, 205.85, 0.25});
-        peak_param.push_back({ 0.96, 249.85, 0.65});
-        peak_param.push_back({ 0.88, 272.85, 3.25});
+        //peak_param.push_back({ 1.02, 122.85, 0.38});
+        //peak_param.push_back({ 0.96, 149.85, 0.40});
+        //peak_param.push_back({ 1.05, 161.85, 0.65});
+        //peak_param.push_back({ 1.40, 188.85, 0.55});
+        //peak_param.push_back({ 1.42, 205.85, 0.25});
+        //peak_param.push_back({ 0.96, 249.85, 0.65});
+        //peak_param.push_back({ 0.88, 272.85, 3.25});
         //vector<double> total_curve(data.first.size());
-        double orig_integral = 0.0;
-        vector<double> orig_fit(data.second.size());
-        for (int d = 0; d < int(data.second.size()); d++) {
-            double fit = 0.0;
-            for (int e = 0; e < int(peak_param.size()); e++) {
-                fit += func(data.first[d], peak_param[e]);
-            }
-            orig_integral += fit;
-            orig_fit[d] = fit;
-        }
-        double orig_fom = 0.0;
-        for (int f = 0; f < int(data.second.size()); ++f) {
-            orig_fom += abs(data.second[f] - orig_fit[f]) / orig_integral;
-        }
-        double area = 0.0;
-        for (int i = 0; i < int(data.first.size()); ++i) {
-            for (int x = 0; x < int(peak_param.size()); ++x) {
-                area += quickFok(data.first[i], peak_param[x]);
-            }
-        }
-        int cons = 1;
-        int iteration = 0;
-        vector<vector<double>> temp_param = peak_param;
-        while (area < 0.97 * curveArea || area > 1.03 * curveArea) {
-            if (iteration > 1000)
-                break;
-            cons = 20;
-            cons *= (1 - (area / curveArea));
-            for (auto& v : temp_param)
-                v[2] += cons;
-            area = 0.0;
-            for (int i = 0; i < int(data.first.size()); ++i) {
-                for (int x = 0; x < int(peak_param.size()); ++x) {
-                    area += quickFok(data.first[i], temp_param[x]);
-                }
-            }
-            iteration++;
-        }
-        double new_integral = 0.0;
-        vector<double> new_fit(data.second.size());
-        for (int d = 0; d < int(data.second.size()); d++) {
-            double fit = 0.0;
-            for (int e = 0; e < int(temp_param.size()); e++) {
-                fit += func(data.first[d], temp_param[e]);
-            }
-            new_integral += fit;
-            new_fit[d] = fit;
-        }
-        double new_fom = 0.0;
-        for (int f = 0; f < int(data.second.size()); ++f) {
-            new_fom += abs(data.second[f] - new_fit[f]) / new_integral;
-        }
+
+        //calculate original integral
+        //double orig_integral = 0.0;
+        //vector<double> orig_fit(data.second.size());
+        //for (int d = 0; d < int(data.second.size()); d++) {
+        //    double fit = 0.0;
+        //    for (int e = 0; e < int(peak_param.size()); e++) {
+        //        fit += func(data.first[d], peak_param[e]);
+        //    }
+        //    orig_integral += fit;
+        //    orig_fit[d] = fit;
+        //}
+        //double orig_fom = 0.0;
+        //for (int f = 0; f < int(data.second.size()); ++f) {
+        //    orig_fom += abs(data.second[f] - orig_fit[f]) / orig_integral;
+        //}
+
+        vector<vector<double>> peak_100;
+        double fom_100 = -1;
+        int iteration_100 = gd_types(data.first, data.second, peak_100, fom_100, 100);
+        vector<vector<double>> peak_200;
+        double fom_200 = -1;
+        int iteration_200 = gd_types(data.first, data.second, peak_200, fom_200, 200);
+        vector<vector<double>> peak_300;
+        double fom_300 = -1;
+        int iteration_300 = gd_types(data.first, data.second, peak_300, fom_300, 300);
+        vector<vector<double>> peak_400;
+        double fom_400 = -1;
+        int iteration_400 = gd_types(data.first, data.second, peak_400, fom_400, 400);
+        vector<vector<double>> peak_700;
+        double fom_700 = -1;
+        int iteration_700 = gd_types(data.first, data.second, peak_700, fom_700, 700);
+        vector<vector<double>> peak_900;
+        double fom_900 = -1;
+        int iteration_900 = gd_types(data.first, data.second, peak_900, fom_900, 900);
+        vector<double> fom_set{ fom_100 , fom_200, fom_300, fom_400, fom_700, fom_900 };
+        int min_fom = min_element(fom_set.begin(), fom_set.end()) - fom_set.begin();
+        if (min_fom == 0)
+            peak_param = peak_100;
+        else if (min_fom == 1)
+            peak_param = peak_200;
+        else if(min_fom == 2)
+            peak_param = peak_300;
+        else if(min_fom == 3)
+            peak_param = peak_400;
+        else if (min_fom == 4)
+            peak_param = peak_700;
+        else
+            peak_param = peak_900;
+
+        //double area = 0.0;
+        //for (int i = 0; i < int(data.first.size()); ++i) {
+        //    for (int x = 0; x < int(peak_param.size()); ++x) {
+        //        area += quickFok(data.first[i], peak_param[x]);
+        //    }
+        //}
+        //int cons = 1;
+        //int iteration = 0;
+        //vector<vector<double>> temp_param = peak_param;
+        //while (area < 0.97 * curveArea || area > 1.03 * curveArea) {
+        //    if (iteration > 1000)
+        //        break;
+        //    cons = 20;
+        //    cons *= (1 - (area / curveArea));
+        //    for (auto& v : temp_param)
+        //        v[2] += cons;
+        //    area = 0.0;
+        //    for (int i = 0; i < int(data.first.size()); ++i) {
+        //        for (int x = 0; x < int(peak_param.size()); ++x) {
+        //            area += quickFok(data.first[i], temp_param[x]);
+        //        }
+        //    }
+        //    iteration++;
+        //}
+        //double new_integral = 0.0;
+        //vector<double> new_fit(data.second.size());
+        //for (int d = 0; d < int(data.second.size()); d++) {
+        //    double fit = 0.0;
+        //    for (int e = 0; e < int(temp_param.size()); e++) {
+        //        fit += func(data.first[d], temp_param[e]);
+        //    }
+        //    new_integral += fit;
+        //    new_fit[d] = fit;
+        //}
+        //double new_fom = 0.0;
+        //for (int f = 0; f < int(data.second.size()); ++f) {
+        //    new_fom += abs(data.second[f] - new_fit[f]) / new_integral;
+        //}
         //double cur_fom = 0.0;
         //for (int f = 0; f < int(total_curve.size()); ++f) {
         //    cur_fom += abs(data.second[f] - total_curve[f]) / total;
         //}
-        double final_cons = temp_param[0][2] / peak_param[0][2];
-        file1 << filename << " ";
-        file1 << "orig_fom: " << orig_fom << " new_fom: " << new_fom << " ratio: " << final_cons << " iterations: " << iteration << " max_intensity: " << max_intensity << endl;
+        //double final_cons = temp_param[0][2] / peak_param[0][2];
+        //file1 << filename << " ";
+        //file1 << "orig_fom: " << orig_fom << " new_fom: " << new_fom << " ratio: " << final_cons << " iterations: " << iteration << " max_intensity: " << max_intensity << endl;
         //vector<vector<double>> GDParams = peakParams;
         //vector<vector<double>> GDcurve;
         //double fom = 1;
